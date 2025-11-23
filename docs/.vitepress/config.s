7d95a8a3d5015cd5ec632c4e9cdee3a1a3819f0c
Ludwig <luisrahm37@gmail.com>
2025-11-23 18:44:21 +0100

    Update config.s

--- docs/.vitepress/config.s
+++ docs/.vitepress/config.s
@@ -1,7 +1,7 @@
 import { getSidebar } from "vitepress-plugin-auto-sidebar";
 
 export default {
-  base: "/",
+  base: "/sns-docs/",
   title: "SNS-DOCS",
   description: "Docs damit Dusk und Callahan nicht mich fragen",
   lastUpdated: true,
